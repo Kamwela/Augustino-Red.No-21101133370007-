95dff16069a364ab7f753ac31e6e5b9960a6ca37
Kamwela <130525297+Kamwela@users.noreply.github.com>
2023-05-14 03:23:12 -0600

    Delete Gift_Card/Gift_Card directory

--- Gift_Card/Gift_Card/src/main/java/com/GiftCard/Gift_Card/repository/Gift_CardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.GiftCard.Gift_Card.repository;
-
-import com.GiftCard.Gift_Card.entity.Gift_CardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Gift_CardRepository extends JpaRepository<Gift_CardEntity, Long> {
-
-}
